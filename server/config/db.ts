a555c870bb3e3e0777401ff874d2994fd7715f8c
Tucker <trblackw@ncsu.edu>
2019-08-21 18:57:17 -0400

    correct mongodb url

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const DB_URI = `mongodb://localhost:27017/graphql-app`;
+const DB_URI = `mongodb://localhost:27017/gql-chat-demo`;
 // mongoose.setDebug('true');
 mongoose.connect(DB_URI, { useNewUrlParser: true });
 mongoose.connection.once('open', () => console.log('Connected to a MongoDB instance'));
