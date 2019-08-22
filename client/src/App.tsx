030fd76a327075b8380e9c70558827439003a9e3
Tucker <trblackw@ncsu.edu>
2019-08-21 20:53:31 -0400

    Set up basic routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,57 +1,13 @@
 import React, { useState, ChangeEvent, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useQuery, useMutation } from '@apollo/react-hooks';
-import gql from 'graphql-tag';
+import { Router } from '@reach/router';
+import ChatBox from './components/ChatBox';
 
-const GET_MESSAGES = gql`
-   {
-      messages {
-         author
-      }
-   }
-`;
-
-const ADD_BOOK = gql`
-   mutation AddBook($title: String!, $author: String!) {
-      addBook(title: $title, author: $author) {
-         title
-         author
-      }
-   }
-`;
-
-const App: React.FC = () => {
-   const { loading, error, data } = useQuery(GET_MESSAGES);
-   // const [addBook, { data: book }] = useMutation(ADD_BOOK);
-   // const [newBook, setNewBook] = useState<{ title: string; author: string }>({ title: '', author: '' });
-
-   // useEffect(() => {
-   //    console.log('ADDED BOOK', book)
-   // }, [book])
-
-   // const handleInput = (e: ChangeEvent<HTMLInputElement>): void => {
-   //    e.persist()
-   //    setNewBook(prevState => ({ ...prevState, [e.target.name]: e.target.value }));
-   // };
-
-   // const handleAddBook = () => {
-   //    debugger;
-   //    addBook({ variables: { title: newBook.title, author: newBook.author }})
-   // }
-   if (!error) {
-      console.log(data);
-   }
-   // return loading ? (
-   //    <div>Loading...</div>
-   // ) : 
-   return loading ? <div>Loading...</div> : (
-      <div className='App'>
-         <header className='App-header'>
-           <div>hey</div>
-         </header>
-      </div>
-   );
-};
+const App: React.FC = () => (
+   <Router>
+      <ChatBox path="/" />
+   </Router>
+)
 
 export default App;
