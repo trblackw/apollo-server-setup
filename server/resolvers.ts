b0f61b9aef011c667f83f09ad091133aa565974a
Tucker <trblackw@ncsu.edu>
2019-08-24 14:22:56 -0400

    Add additional resolvers

--- server/resolvers.ts
+++ server/resolvers.ts
@@ -1,14 +1,31 @@
-import { MessageType } from "./types";
+import { MessageType } from './types';
 const Message = require('./models/message');
+const { PubSub } = require('apollo-server')
 
+const pubsub = new PubSub();
+const MESSAGE_SENT = 'MESSAGE_SENT';
 module.exports = {
    Query: {
       messages: () => Message.find({})
    },
    Mutation: {
-      addMessage: (_: any, message: MessageType) => {
-         const newMessage = new Message({ author: message.author, body: message.body });
-         return newMessage.save();
+      sendMessage: async (_: any, { from, message }: MessageType) => {
+         const newMessage = await Message.create({ from, message, created_at: new Date() });
+         await pubsub.publish(MESSAGE_SENT, { messageSent: newMessage });
+         return newMessage;
+      },
+      clearMessages: () => {
+         try {
+            Message.collection.drop();
+            return 'SUCCESS';
+         } catch (error) {
+            return 'FAILURE';
+         }
+      }
+   },
+   Subscription: {
+      messageSent: {
+         subscribe: () => pubsub.asyncIterator([MESSAGE_SENT])
       }
    }
 };
