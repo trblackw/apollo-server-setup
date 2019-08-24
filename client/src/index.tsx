6dce83dd2dbf61b36a861546f03e6f2da0947223
Tucker <trblackw@ncsu.edu>
2019-08-24 14:22:41 -0400

    Set up websocket linking

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,14 +4,37 @@ import './index.css';
 import App from './App';
 import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { setContext } from 'apollo-link-context';
 import { createHttpLink } from 'apollo-link-http';
 import { ApolloProvider } from '@apollo/react-hooks';
+import { WebSocketLink } from 'apollo-link-ws';
+import { split, ApolloLink } from 'apollo-link';
+import { getMainDefinition } from 'apollo-utilities';
+const gqlPathName = 'localhost:4000/graphql';
 
-const link = createHttpLink({
-   uri: 'http://localhost:3001/graphql'
+const httpLink = createHttpLink({
+   uri: `http://${gqlPathName}`
 });
- 
+
+const wsLink = new WebSocketLink({
+   uri: `ws://${gqlPathName}`,
+   options: {
+      reconnect: true
+      //   lazy: true
+   }
+});
+
+const terminatingLink = split(
+   // split based on operation type
+   ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+   },
+   wsLink,
+   httpLink
+);
+
+const link = ApolloLink.from([terminatingLink]);
+
 const client = new ApolloClient({
    link,
    cache: new InMemoryCache()
